54a42ec30cd4e845814c3b82ebb2624f42eb7d3d
Yulei-cn <116038178+Yulei-cn@users.noreply.github.com>
2023-01-10 15:54:55 +0100

    Update dichotomique.py

--- dichotomique.py
+++ dichotomique.py
@@ -39,4 +39,34 @@ else:
 
     
 #dichotomique    itératif
+def inputList():
+    #num_list = list(map(int, input().split()))  # 或者
+    print("请输入整数，以空格隔开")
+    num_list = [int(i) for i in input().split()] # 接收一段以空格隔开的整数的输入
+    return num_list
+def findMin(list):
+    num_min = list[0] #记录最小值
+    j = 0  #记录取得最小值时的下标
+    for i in range(len(list)):
+        if list[i] <= num_min:
+            num_min = list[i]
+            j = i
+    return num_min,j
+def selectionSort(list):
+    new_list = [] #排好序的列表
+    for i in range(len(list)):
+        min, j = findMin(list)
+        new_list.append(min)
+        del list[j]
+    return new_list
+def outputList(list):
+    for i in range(len(list)):
+        print(list[i])
+if __name__ == '__main__':
+    list = inputList()
+    list = selectionSort(list)
+    print(list)
+    outputList(list)
+    #min,j = findMin(list)
+    #print("最小值为：%d，对应的下标为%d"%(min,j))
 
